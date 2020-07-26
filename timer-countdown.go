6d223f361223ea6ccc802f0600fc6ade1e9a2430
Kehula <kehula1@gmail.com>
2020-07-26 13:08:41 +0300

    Update timer-countdown.go

--- timer-countdown.go
+++ timer-countdown.go
@@ -18,3 +18,15 @@ func main() {
 func launch() {
 	fmt.Println("Lift off!")
 }
+
+
+//var tick <-chan time.Time
+//tick = time.Tick(500 * time.Millisecond)
+//select {
+//case "something":
+//	...
+//case <-tick:
+//	...
+//case <-time.After(1 * time.Second):
+//	...
+//}
